11feeca97243d59b7f12956d0c8d7d265ccf2e59
Hosseyn <hosseyn@Elahehs-Mac-mini.local>
2024-09-14 08:17:48 +0200

    fix

--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -1,3 +1,5 @@
-export default function handler(req: any, res: any) {
+import {NextApiRequest, NextApiResponse} from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
     res.status(200).json({name: "John Doe"});
 }
