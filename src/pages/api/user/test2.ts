51eda9069dd4fd82d014079d5af2fd856ad45187
Hosseyn <hosseyn@Elahehs-Mac-mini.local>
2024-09-14 19:11:48 +0200

    fix

--- src/pages/api/user/test2.ts
+++ src/pages/api/user/test2.ts
@@ -1,5 +1,7 @@
 import type {NextApiRequest, NextApiResponse} from "next";
+import {methodHelper} from "./login";
 
 export default function login(req: NextApiRequest, res: NextApiResponse) {
+    methodHelper(req, res, "POST");
     res.status(200).json({name: "John Doe2"});
 }
